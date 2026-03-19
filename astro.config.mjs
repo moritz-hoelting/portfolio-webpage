bbdc4ea50eed597c958feb59867c46691460877d
Moritz Hölting <87192362+moritz-hoelting@users.noreply.github.com>
2026-03-20 00:09:31 +0100

    add blog on doomsday algorithm

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,28 +7,49 @@ import rehypeKatex from "rehype-katex";
 import matomo from "./src/integrations/matomo";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hoelting.dev",
-  trailingSlash: "always",
+    site: "https://hoelting.dev",
+    trailingSlash: "always",
 
-  integrations: [mdx(), sitemap(), icon(), solidJs(), matomo({
-    enabled: import.meta.env.PROD, // only enable in production
-    url: "https://analytics.hoelting.dev",
-    siteId: 3,
-    disableCookies: true,
-    enableCrossDomainLinking: true,
-    domains: ["*.hoelting.dev", "*.www.hoelting.dev"],
-    respectDoNotTrack: true,
-  })],
+    integrations: [
+        sitemap(),
+        mermaid({
+            autoTheme: true,
+            mermaidConfig: {
+                themeVariables: {
+                    fontFamily: '"FiraCode", monospace',
+                },
+            },
+        }),
+        mdx(),
+        icon(),
+        solidJs(),
+        matomo({
+            enabled: import.meta.env.PROD, // only enable in production
+            url: "https://analytics.hoelting.dev",
+            siteId: 3,
+            disableCookies: true,
+            enableCrossDomainLinking: true,
+            domains: ["*.hoelting.dev", "*.www.hoelting.dev"],
+            respectDoNotTrack: true,
+        }),
+    ],
 
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    },
 
-  vite: {
-    plugins: [tailwindcss({ applyBaseStyles: false })],
-  },
+    vite: {
+        plugins: [tailwindcss({ applyBaseStyles: false })],
+        server: {
+            watch: {},
+        },
+        optimizeDeps: {
+            include: ["mermaid"],
+        },
+    },
 });
